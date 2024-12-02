e4c37347515dbeb8ab55271ed7785a351e6ef7f9
Susan Fung <38925660+susanfung@users.noreply.github.com>
2024-12-02 10:53:17 -0700

    Update Day1Test.java

--- src/test/java/org/samples/Day1Test.java
+++ src/test/java/org/samples/Day1Test.java
@@ -45,4 +45,17 @@ public class Day1Test {
 
         Approvals.verify(result);
     }
+
+    @Test
+    public void testDay1Part2_similarityScore() {
+        Day1 day1 = new Day1();
+        String filePath = "./src/test/java/org/samples/Day1TestData.txt";
+
+        int[] column1 = day1.parseColumn(filePath, 1);
+        int[] column2 = day1.parseColumn(filePath, 2);
+
+        int result = day1.similarityScore(column1, column2);
+
+        Approvals.verify(result);
+    }
 }
