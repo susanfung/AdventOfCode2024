a5376ca122ee52aa1985635256b439d27fb3a26a
Susan Fung <38925660+susanfung@users.noreply.github.com>
2024-12-02 10:34:39 -0700

    Create Day1Test.java

---
+++ src/test/java/org/samples/Day1Test.java
@@ -0,0 +1,35 @@
+package org.samples;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class Day1Test {
+    @Test
+    public void testDay1Part1_parseColumn() {
+        Day1 day1 = new Day1();
+        String filePath = "src/test/java/org/samples/Day1TestData.txt";
+
+        int[] column1 = day1.parseColumn(filePath, 1);
+        int[] column2 = day1.parseColumn(filePath, 2);
+
+        String result = "Column 1: " + Arrays.toString(column1) + "\n" +
+                "Column 2: " + Arrays.toString(column2);
+
+        Approvals.verify(result);
+    }
+
+    @Test
+    public void testDay1Part1_part1Answer() {
+        Day1 day1 = new Day1();
+        String filePath = "./src/test/java/org/samples/Day1TestData.txt";
+
+        int[] column1 = day1.parseColumn(filePath, 1);
+        int[] column2 = day1.parseColumn(filePath, 2);
+
+        int result = day1.part1Answer(column1, column2);
+
+        Approvals.verify(result);
+    }
+}
