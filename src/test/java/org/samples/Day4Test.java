3582d0689b800abeb90eb5c61b8b717e4c60c658
Susan Fung <38925660+susanfung@users.noreply.github.com>
2024-12-04 08:51:08 -0700

    Update Day4Test.java

--- src/test/java/org/samples/Day4Test.java
+++ src/test/java/org/samples/Day4Test.java
@@ -15,4 +15,14 @@ public class Day4Test {
 
         Approvals.verify(result);
     }
+
+    @Test
+    public void testDay4Part1_answer() throws IOException {
+        Day4 day4 = new Day4();
+        String filePath = "src/test/java/org/samples/Day4Data.txt";
+
+        String result = day4.findXmasWithMarkers(filePath);
+
+        Approvals.verify(result);
+    }
 }
